34c1309cc4b85e53971083b9fe1a79e7b6dfdf6f
houfazhi <2775313450@qq.com>
2023-12-13 15:04:38 +0800

    add .gitignore(new) and some basic knowledge about python

--- tts/demo.py
+++ tts/demo.py
@@ -1,8 +1,9 @@
 import pyttsx3
-with open("../file/a.txt", 'r', encoding='utf-8') as rfile:
-        lyric = rfile.read()
-engine = pyttsx3.init()
-if lyric:
-    engine.say(lyric)
-    engine.runAndWait()
-    engine.stop()
+if __name__ == "__main__":
+    with open("../file/a.txt", 'r', encoding='utf-8') as rfile:
+            lyric = rfile.read()
+    engine = pyttsx3.init()
+    if lyric:
+        engine.say(lyric)
+        engine.runAndWait()
+        engine.stop()
--- tts/demo1.py
+++ tts/demo1.py
@@ -10,24 +10,26 @@ import os
 # else:
 #     print("file not found")
 #     exit()
-with open("../file/a.txt", 'r', encoding='utf-8') as rfile:
-    lyric = rfile.read()
+if __name__ == "__main__":
+    with open("../file/a.txt", 'r', encoding='utf-8') as rfile:
+        lyric = rfile.read()
 
-if lyric:
-    # lyric = "They say that life is always easier"
-    # 创建gTTS对象
-    tts = gTTS(text=lyric, lang='en', slow=False)
+    if lyric:
+        # lyric = "They say that life is always easier"
+        # 创建gTTS对象
+        tts = gTTS(text=lyric, lang='en', slow=False)
 
-    # output_file = os.path.expanduser("~/Documents/lyric.mp3")
-    output_file = "lyric.mp3"
+        # output_file = os.path.expanduser("~/Documents/lyric.mp3")
+        output_file = "lyric.mp3"
 
-    # 保存音频文件
-    tts.save(output_file)
+        # 保存音频文件
+        tts.save(output_file)
 
-#    os.system("start " + output_file)
-    os.system(output_file)
-    # 播放音频文件
-    # playsound(output_file)
-else:
-    print("lyric is empty")
+        #    os.system("start " + output_file)
+        os.system(output_file)
+        # 播放音频文件
+        # playsound(output_file)
+    else:
+        print("lyric is empty")
+
 
